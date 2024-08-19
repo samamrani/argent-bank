0039224e0723df94d673fe6068c998734c019356
atmani samira <erp.atmanis@gmail.com>
2024-08-19 18:34:53 +0200

    Ajout de la redirection vers la page d'accueil si l'utilisateur n'est pas authentifié

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import UserProfile from "../components/ProfileForm";
 import ProfilePage from "../components/ProfilePage";
+import { useSelector } from 'react-redux';
+import {Navigate} from "react-router-dom";
+
 
 function Profile(){
+    const {token} = useSelector((state) => state.user);
+    if(!token){
+        return <Navigate to="/"/>
+    }
     return(
         <main className="bg-dark">
             <UserProfile/>
