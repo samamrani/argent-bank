69a6e917ff655ec92066350255e8a59650909618
atmani samira <erp.atmanis@gmail.com>
2024-08-31 01:11:10 +0200

    REVOIR LE CODE

--- src/services/logoutService.js
+++
@@ -1,7 +0,0 @@
-
-  // Déconnecte l'utilisateur en supprimant le token d'authentification des stockages.
-
-export const logout = () => {
-  localStorage.removeItem('userToken');
-  sessionStorage.clear();
-};
